2fd20fc98298567c3bbf3f735700475975ce38be
jardianoalmeida <jardiano.almeida@gmail.com>
2020-11-06 19:27:25 -0400

    Implemetando o ViewModel

--- app/src/main/java/br/com/jardiano/countriesapp/viewmodel/ListViewModel.java
+++ app/src/main/java/br/com/jardiano/countriesapp/viewmodel/ListViewModel.java
@@ -1,4 +1,34 @@
 package br.com.jardiano.countriesapp.viewmodel;
 
-public class ListViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.jardiano.countriesapp.model.CountryModel;
+
+public class ListViewModel extends ViewModel {
+    public MutableLiveData<List<CountryModel>> countries = new MutableLiveData<List<CountryModel>>()
+    public MutableLiveData<Boolean> countryLoadErro = new MutableLiveData<Boolean>();
+    public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
+
+    protected void refresh(){
+        fetchCountries();
+    }
+
+    protected void fetchCountries(){
+        CountryModel country1 = new CountryModel("Albania", "Tirana", "");
+        CountryModel country2 = new CountryModel("Brasil", "Brasil", "");
+        CountryModel country3 = new CountryModel("Czechia", "Praba", "");
+
+        List<CountryModel> list = new ArrayList<>();
+        list.add(country1);
+        list.add(country2);
+        list.add(country3);
+
+        countries.setValue(list);
+        countryLoadErro.setValue(false);
+        loading.setValue(false);
+    }
 }
